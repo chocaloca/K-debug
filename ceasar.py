9f1757857a4fb1c8a5c6f9e2fc0585fb3acf0c5a
Jack <j.winnd@gmail.com>
2024-02-11 12:57:51 -0500

    Remove debug statements

--- ceasar.py
+++ ceasar.py
@@ -8,7 +8,6 @@ shift = int(input("Type the shift number:\n"))
 
 message = []
 def caesar(text, shift):
-  print("text: ",text,"\nshift: ",shift)
   message = list(text)
   # if shift > 26:
   #   shift = shift % 26
@@ -16,31 +15,20 @@ def caesar(text, shift):
   for i in message:
     shift_new = shift
     if i in alphabet:
-      print("message loop start: ",message)
       position = message.index(i)
-      print("position: ",position)
       location = alphabet.index(i)
-      print(" location: ",location)
   #     if direction == "decode":
   #       shift = shift * -1
-      print(" shift-pre: ",shift)
       shift_new = shift + location
       if shift_new > 26:
         shift_new %= 26
-      
-      print(" shift post-mod: ",shift)
-      
+
       # newlet = alphabet[location + shift]
       newlet = alphabet[shift_new] 
-      print("  newlet: ",newlet)
-      
       message[count]=newlet
-      
       # message.insert(position,newlet)
-      print("message-new:        ",message)
       # message.remove(i)
-      # print("message-new-2: ",message)
-      # print(message)
+      print(message)
       count += 1
   
   
