b3d4c7d42fe60cb727d0e5251524a44587984e17
Jack <j.winnd@gmail.com>
2024-02-11 12:54:26 -0500

    Debug

--- ceasar.py
+++ ceasar.py
@@ -1,4 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+            'k', 'l', 'm', 'n', 'o', 'p',  'q', 'r', 's', 't', 
+            'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -8,10 +10,11 @@ message = []
 def caesar(text, shift):
   print("text: ",text,"\nshift: ",shift)
   message = list(text)
-  print("message: ",message)
-  if shift > 26:
-    shift = shift % 26
+  # if shift > 26:
+  #   shift = shift % 26
+  count = 0
   for i in message:
+    shift_new = shift
     if i in alphabet:
       print("message loop start: ",message)
       position = message.index(i)
@@ -20,13 +23,25 @@ def caesar(text, shift):
       print(" location: ",location)
   #     if direction == "decode":
   #       shift = shift * -1
-      newlet = alphabet[location + shift] 
+      print(" shift-pre: ",shift)
+      shift_new = shift + location
+      if shift_new > 26:
+        shift_new %= 26
+      
+      print(" shift post-mod: ",shift)
+      
+      # newlet = alphabet[location + shift]
+      newlet = alphabet[shift_new] 
       print("  newlet: ",newlet)
-      message.insert(position,newlet)
-      print("message-new-1: ",message)
-      message.remove(i)
-      print("message-new-2: ",message)
+      
+      message[count]=newlet
+      
+      # message.insert(position,newlet)
+      print("message-new:        ",message)
+      # message.remove(i)
+      # print("message-new-2: ",message)
       # print(message)
+      count += 1
   
   
 caesar(text, shift)
